11a956f5124c9a7fd94ca652b998d466facf36e6
finile <wu.nile@gmail.com>
2017-12-03 09:54:26 +0800

    user can update category with the dame form on index page

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -4,7 +4,11 @@ class Admin::CategoriesController < ApplicationController
 
   def index
     @categories = Category.all
-    @category = Category.new
+      if params[:id]
+        @category = Category.find(params[:id])
+      else
+      @category = Category.new
+    end
   end
 
   def create
@@ -18,6 +22,17 @@ class Admin::CategoriesController < ApplicationController
     end
   end 
 
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      redirect_to admin_categories_path
+      flash[:notice] = "Category was successfully updated"
+    else
+      @category = Category.all
+      render :index
+    end
+  end
 
 private
   
